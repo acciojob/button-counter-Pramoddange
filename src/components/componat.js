a2fd068b5d03d7ddeefac7dcca632a42ce5a8001
Pramod Dange <pramodsdange1@gmail.com>
2024-08-24 19:25:03 +0530

    done

--- src/components/componat.js
+++ src/components/componat.js
@@ -1,20 +1,23 @@
-import React,{ Componet } from "react";
+import React,{ Component } from "react";
 
-class Counter extend Componet{
+class Counter extends Component {
 constructor(props){
     super(props);
     this.state={counter:0}
 }
-incerementCount=()=>{
-    const cont=this.state.counter;
-   this.setSate({counter:cont+1});
+
 render(){
+    incerementCount=()=>{
+        const cont=this.state.counter;
+       this.setSate({counter:cont+1});
+    }
     return (
         <>
         <p>Button clicked {this.state.counter} times</p>
         <button onClick={this.incerementCount}>Click me</button>
         </>
-    )
+    );
 }
 }
+
 export default Counter;
